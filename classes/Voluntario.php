339287d83478dd8e2be46f9169c2b0f0100b3df6
gabrielknust <gabrielsilvaknustws@hotmail.com>
2018-06-05 17:40:27 -0300

    05/06/2018-Modificação nas classes Voluntario e Funcionario

--- classes/Voluntario.php
+++ classes/Voluntario.php
@@ -32,6 +32,76 @@ class Voluntario extends Pessoa
     {
         $this->tipo = $tipo;
     }
+    //Insert
+    public function incluir($idvoluntario,$tipo) {
+        try {
+            $sql = 'INSERT cargo (idvoluntario,tipo) VALUES (:idvoluntario, :tipo)';
+            $sql = str_replace("'", "\'", $sql);
+            $acesso = new Acesso();
+            
+            $pdo = $acesso->conexao();
+            
+            $stmt = $pdo->prepare($sql);
+            
+            $stmt->bindParam(':idfuncionario', $idfuncionario);
+            $stmt->bindParam(':tipo', $tipo);
+            $stmt->execute();
+            
+        } catch (PDOException $e) {
+            echo 'Error: <b>  na tabela pessoas = ' . $sql . '</b> <br /><br />' . $e->getMessage();
+        }
+    }
+    
+    //excluir
+    public function excluir($idcargo) {
+        try {
+            $sql = 'DELETE from cargo WHERE idcargo = :idcargo';
+            $sql = str_replace("'", "\'", $sql);
+            $acesso = new Acesso();
+            
+            
+            $pdo = $acesso->conexao();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $stmt = $pdo->prepare($sql);
+            
+            $stmt->bindParam(':idcargo', $idcargo);
+            
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo 'Error: <b>  na tabela pessoas = ' . $sql . '</b> <br /><br />' . $e->getMessage();
+        }
+    }
+    
+    //Editar
+    public function alterar($idfuncionario,$tipo,$carga_mensal,$primeira_entrada,$primeira_saida,$segunda_entrada,$segunda_saida,$carga_diaria,$dias_trabalhados,$folgas) {
+        try {
+            $sql = 'update pessoas set idfuncionario=:idfuncionario, idpessoa=:idpessoa, idcargo=:idcargo, imagem=:imagem, vale_transporte=:vale_transporte, data_admissao=:data_admissao, registro_geral=:registro_geral, orgao_emissor=:orgao_emissor, data_expedicao=:data_expedicao, pis=:pis, ctps=:ctps, uf_ctps=:uf_ctps, zona=:zona, certificado_reservista_numero=:certificado_reservista_numero, nome_mae=:nome_mae, nome_pai=:nome_pai';
+            $sql = str_replace("'", "\'", $sql);
+            $acesso = new Acesso();
+            
+            
+            $pdo = $acesso->conexao();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $stmt = $pdo->prepare($sql);
+            
+            $stmt->bindParam(':idfuncionario', $idfuncionario);
+            $stmt->bindParam(':tipo', $tipo);
+            $stmt->execute();
+            
+        } catch (PDOException $e) {
+            echo 'Error: <b>  na tabela pessoas = ' . $sql . '</b> <br /><br />' . $e->getMessage();
+        }
+    }
+    //Consultar
+    public function consultar($sql) {
+        $acesso = new Acesso();
+        $acesso->conexao();
+        $acesso->query($sql);
+        $this->Linha = $acesso->linha;
+        $this->Result = $acesso->result;
+    }
     
 }
 
