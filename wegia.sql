16caebce1cbdc4746faa6b1355eab1098638d058
GuiAzis <guilherme_azis@hotmail.com>
2018-08-03 17:52:21 -0300

    atualização dia 03/08

--- wegia.sql
+++ wegia.sql
@@ -4,72 +4,71 @@ use wegia;
 
 create table pessoa (
 	id_pessoa int not null primary key auto_increment,
-    
-    cpf varchar(40), 
+    cpf varchar(40), #CPF da pessoa
     senha varchar(70),
-    nome varchar(100) not null,
-    sexo varchar(10) not null,
-    telefone varchar(33),
-    data_nascimento date not null,
+    nome varchar(100),
+    sexo char(1),
+    telefone varchar(25),
+    data_nascimento date,
     imagem longtext,
-    cep int not null,
-    cidade varchar(40) not null,
-    bairro varchar(40) not null,
-    rua varchar(40) not null,
-    numero_endereco varchar(7),
+    cep varchar(10),
+    cidade varchar(40),
+    bairro varchar(40),
+    logradouro varchar(40),
+    numero_endereco varchar(10),
     complemento varchar(50),
-	registro_geral varchar(13) not null, 
-    orgao_emissor varchar(20) not null,
+	registro_geral varchar(13), 
+    orgao_emissor varchar(20),
     data_expedicao date,
     nome_mae varchar(100),
     nome_pai varchar(100)
     
 )engine = InnoDB;
 
+create table situacao_interno(
+	id_situacao_interno int not null primary key auto_increment,
+    data_hora datetime,
+    descricao varchar(50)
+    
+)engine = InnoDB;
+
 create table interno(
-	id_interno int not null primary key,
-    id_pessoa int not null,
-    id_situacao_interno int not null,
+	id_interno int not null primary key auto_increment,
+    id_pessoa int,
+    id_situacao_interno int,
     
     nome_contato_urgente varchar(60),
     telefone_contato_urgente_1 varchar(33),
     telefone_contato_urgente_2 varchar(33),
     telefone_contato_urgente_3 varchar(33),
     
-    foreign key(id_pessoa) references pessoa(id_pessoa)
+    foreign key(id_pessoa) references pessoa(id_pessoa),
+    foreign key(id_situacao_interno) references situacao_interno(id_situacao_interno)
     
 )engine = InnoDB;
 
-create table situacao_interno(
-	id_situacao_interno int not null primary key,
-    
-    descricao varchar (50) not null
-    
-)engine = InnoDB;
 
 create table movimentacao_interno(
-	id_movimentacao int not null primary key,
     id_interno int not null,
     id_situacao_interno int not null,
     
-    data_hora date not null,
-    
+	primary key(id_interno,id_situacao_interno),
     foreign key(id_interno) references interno(id_interno),
     foreign key(id_situacao_interno) references situacao_interno(id_situacao_interno)
     
 )engine = InnoDB;
 
 create table voluntario(
-	id_voluntario int not null primary key,
-    id_pessoa int not null,
+	id_voluntario int not null primary key auto_increment,
+    id_pessoa int,
     
     foreign key(id_pessoa) references pessoa(id_pessoa)
     
 )engine = InnoDB;
 
 create table voluntario_judicial(
-	id_voluntario_judicial int not null primary key,
-    id_pessoa int not null,
+	id_voluntario_judicial int not null primary key auto_increment,
+    id_pessoa int,
     
 	documento_judicial varchar(40),
     foreign key(id_pessoa) references pessoa(id_pessoa)
@@ -91,11 +90,11 @@ create table voluntario_judicial(
 	observacoes varchar(240)
 
  )engine = InnoDB;
- 
+
 create table funcionario(
 	id_funcionario int not null primary key auto_increment,
-    id_pessoa int not null,
-    id_quadro_horario int not null,
+    id_pessoa int,
+    id_quadro_horario int,
     
     vale_transporte varchar(16),
     data_admissao date not null,
@@ -125,27 +124,24 @@ funcionários, se este existir ele reconhecerá que a conta a ser criada pertenc
 Se não for encontrado esse CPF na tabela funcionário, não poderá ser criada a conta */
 
 create table situacao_funcionario(
-	id_situacao_funcionario int not null primary key,
-    
-    descricao varchar (50) not null
+	id_situacao_funcionario int not null primary key auto_increment,
+	data_hora datetime,
+    descricao varchar (50)
     
 )engine = InnoDB;
 
 create table movimentacao_funcionario(
-	id_movimentacao int not null primary key,
     id_funcionario int not null,
     id_situacao_funcionario int not null,
     
-    data_hora date not null,
-    
+	primary key(id_funcionario,id_situacao_funcionario),
 	foreign key(id_funcionario) references funcionario(id_funcionario),
     foreign key(id_situacao_funcionario) references situacao_funcionario(id_situacao_funcionario)
     
 )engine = InnoDB;
 
 create table cargo(
-	id_cargo int not null primary key,
-    
+	id_cargo int not null primary key auto_increment,
     descricao varchar(30)
     
 )engine = InnoDB; /* O cargo que o usuário tiver definirá a quais tabelas ele terá acesso e quais não, se é possível 
@@ -200,12 +196,3 @@ create table voluntario_judicial_cargo(
     foreign key(id_cargo) references cargo(id_cargo),
     foreign key(id_voluntarioJ) references voluntario_judicial(id_voluntario_judicial)
 )engine = InnoDB;
-
-delimiter $$
-create procedure inserir(in $id_quadro_horario int,in $cpf varchar(40),in $senha varchar(70),in $nome varchar(100),in $sexo varchar(10),in $telefone varchar(33),in $data_nascimento date,in $imagem longtext,in $cep int,in $cidade varchar(40),in $bairro varchar(40),in $rua varchar(40),in $numero_endereco varchar(7),in $complemento varchar(50),in $registro_geral varchar(13),in $orgao_emissor varchar(20),in $data_expedicao date,in $nome_mae varchar(100),in $nome_pai varchar(100),in $vale_transporte varchar(16),in $data_admissao date,in $pis varchar(14),in $ctps varchar(15),in $uf_ctps varchar(2),in $numero_titulo varchar(15),in $zona varchar(3),in $secao varchar(4),in $certificado_reservista_numero int,in $certificado_reservista_serie varchar(10),in $calcado varchar(2),in $calca varchar(2),in $jaleco varchar(2),in $camisa varchar(2),in $usa_vtp varchar(3),in $cesta_basica varchar(3),in $situacao varchar(10), in $escala varchar(100),in $tipo varchar(20),in $carga_horaria varchar(50),in $entrada1 varchar(50),in $saida1 varchar(50),in $entrada2 varchar(50),in $saida2 varchar(50),in $total varchar(50),in $dias_trabalhados varchar(50),in $folga varchar(50),in $observacoes varchar(50),in $id_pessoa int)
-begin
-	INSERT INTO pessoa (cpf, senha, nome, sexo, telefone, data_nascimento, imagem, cep, cidade, bairro, rua, numero_endereco, complemento, registro_geral, orgao_emissor, data_expedicao, nome_mae, nome_pai) VALUES ($cpf,$senha,$nome,$sexo,$telefone,$data_nascimento,$imagem,$cep,$cidade,$bairro,$rua,$numero_endereco,$complemento,$registro_geral,$orgao_emissor,$data_expedicao,$nome_mae,$nome_pai);
-    insert into quadro_horario(escala,tipo,carga_horaria,entrada1,saida1,entrada2,saida2,total,dias_trabalhados,folga,observacoes) values ($escala,$tipo,$carga_horaria,$entrada1,$saida1,$entrada2,$saida2,$total,$dias_trabalhados,$folga,$observacoes);
-    insert into funcionario(id_pessoa, id_quadro_horario,vale_transporte,data_admissao,pis,ctps,uf_ctps,numero_titulo,zona,secao,certificado_reservista_numero,certificado_reservista_serie,calcado,calca,jaleco,camisa,usa_vtp,cesta_basica,situacao) values ($id_pessoa,$id_quadro_horario,$vale_transporte,$data_admissao,$pis,$ctps,$uf_ctps,$numero_titulo,$zona,$secao,$certificado_reservista_numero,$certificado_reservista_serie,$calcado,$calca,$jaleco,$camisa,$usa_vtp,$cesta_basica,$situacao);
-end $$
-delimiter ;
