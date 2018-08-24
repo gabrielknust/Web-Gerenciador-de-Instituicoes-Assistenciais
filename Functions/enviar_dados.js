e2c0fad533053ca7abbd97b687733b050df4c95b
gabrielknust <gabrielsilvaknustws@hotmail.com>
2018-08-24 17:13:41 -0300

    quase um beta - 3

--- Functions/enviar_dados.js
+++ Functions/enviar_dados.js
@@ -1,21 +1,4 @@
 function enviar_dados(){
 
-	if( $("#nome").val() == "" ){
-		alert("Preencha o campo: Nome");
-		$("#nome").focus();
-
-	}
-
-	else if( $("#telefone").val() == ""){
-
-		alert("Preenchao campo: telefone");
-		$("#telefone").focus();
-	}
-
-	else if( $("#pai").val() == "" ){
-		alert("Preencha o campo: Nome do Pai");
-		$("#pai").focus();
-
-	}
 
 }
