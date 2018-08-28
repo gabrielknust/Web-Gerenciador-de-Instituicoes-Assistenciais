73558087330be264f5f6f849e76d0388a575483b
JuanvictorO <juanvictoroliveira2009@gmail.com>
2018-08-28 17:40:25 -0300

    validação quase completa

--- Functions/enviar_dados.js
+++ Functions/enviar_dados.js
@@ -3,6 +3,9 @@ function enviar_dados(){
 	if($("#sangue").val() == null){
 
 		alert("Selecione o tipo sanguíneo");
-		$("#sangue").focus();
 	}
+	else if($("#senha").val() !=  $("#c_senha").val()){
+    	alert("Senhas estão diferentes");
+    	$("#senha").focus();
+    }
 }
