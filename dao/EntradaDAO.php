ce37726af8062ed3afcb0e8ad43f6c9882caf9ff
JuanvictorO <juanvictoroliveira2009@gmail.com>
2018-12-11 00:59:15 -0200

    Update EntradaDAO.php

--- dao/EntradaDAO.php
+++ dao/EntradaDAO.php
@@ -5,6 +5,36 @@ require_once'../Functions/funcoes.php';
 
 class EntradaDAO
 {
+	public function incluir($entrada){
+        try{
+            extract($_REQUEST);
+            $pdo = Conexao::connect();
+
+            $sql = 'INSERT entrada(id_origem,id_almoxarifado,id_tipo,data,hora,valor_total) VALUES( :id_origem,:id_almoxarifado,:id_tipo,:data,:hora,:valor_total)';
+            $sql = str_replace("'", "\'", $sql);            
+            $stmt = $pdo->prepare($sql);
+
+            $id_origem = $entrada->getId_origem()->getId_origem();
+            $id_almoxarifado = $entrada->getId_almoxarifado()->getId_almoxarifado();
+            $id_tipo = $entrada->getId_tipo()->getId_tipo();
+            $data = $entrada->getData();
+            $hora = $entrada->getHora();
+            $valor_total = $entrada->getValor_total();
+
+            $stmt->bindParam(':id_origem',$id_origem);
+            $stmt->bindParam(':id_almoxarifado',$id_tipo);
+            $stmt->bindParam(':id_tipo',$id_tipo);
+            $stmt->bindParam(':data',$data);
+            $stmt->bindParam(':hora',$hora);
+            $stmt->bindParam(':valor_total',$valor_total);
+
+            $stmt->execute();
+        } catch(PDOExeption $e){
+            echo 'Error: <b>  na tabela produto = ' . $sql . '</b> <br /><br />' . $e->getMessage();
+        }
+
+    }
+	
 	public function listarTodos(){
 
     try{
